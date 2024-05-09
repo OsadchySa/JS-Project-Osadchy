4d277345280bb9d10abe9ba16e74abbc4afb38aa
OsadchySa <osadchyalexandr0790@gmail.com>
2024-05-09 19:08:27 +0300

    fix task4

--- TASK4/task4.js
+++ TASK4/task4.js
@@ -118,7 +118,7 @@ console.log(swap([11,22,33,44],1,2))
 
 //Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
 // Приклад exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD') // => 250
-function exchange(sumUAH,currencyValues,exchangeCurrency) {
+function exchange(sumUAH, currencyValues, exchangeCurrency) {
     for (const carVal of currencyValues) {
         if (carVal.currency === exchangeCurrency) {
             return sumUAH/carVal.value
@@ -126,4 +126,4 @@ function exchange(sumUAH,currencyValues,exchangeCurrency) {
 
     }
 }
-console.log(exchange(8400,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD'))
+console.log(exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'EUR'))
